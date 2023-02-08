59b668ca1afddaee9107df5eac1f0e6191d37832
khrystynadmytriv <dmytriv.pn@ucu.edu.ua>
2023-02-08 17:23:54 +0200

    check rows

---
+++ puzzle.py
@@ -0,0 +1,21 @@
+def first_function(board):
+    """
+    >>> first_function([\
+    "**** ****",\
+    "***1 ****",\
+    "  3****",\
+    "* 4 1****",\
+    "     9 5 ",\
+    " 6  83  *",\
+    "3   1  ",\
+    "  8  2***",\
+    "  2  ****"\
+    ])
+    True
+    """
+    for i in board:
+        lst = [j for j in i if j!= "*" and j!= " "]
+        check = [i.count(j) == 1 for j in lst]
+        if not all(check) or len(lst) > 9:
+            return False
+    return True
